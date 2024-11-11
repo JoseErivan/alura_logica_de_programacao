450ce9beb29a292a4dbbc7c53d22a54fc7b31a6a
Jose Erivan <jose.erivan.ramalho@gmail.com>
2024-11-11 11:20:12 -0300

    complete project

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,29 @@
+// Criamos a mensagem inicial do jogo
 alert("Bem vindo ao jogo do advinhe");
-let chute = prompt("Digite aqui o seu chute:");
-let numeroSecreto = 5;
-if (chute == numeroSecreto) {
-    alert("Você acertou o número secreto!! Número: " + numeroSecreto);
-} else if (chute > numeroSecreto) {
-    alert("O número secreto é menor que o número informado.");
-} else {
-    alert("O número secreto é maior que o número informado");
-} 
-
-console.log("Hello, World!");
+let numeroMaximo = 1000;
+// Definindo o número secreto
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+// Criando variável para armazenar o chute do usuário
+let chute;
+let tentativas = 0;
+// Criando o loop para o jogo
+while (chute != numeroSecreto) {
+    // Pedindo para o usuário digitar o seu chute
+    chute = prompt("Escolha um número de 1 a " + numeroMaximo);
+    // Verificando se o usuário acertou
+    if (chute == numeroSecreto) {
+        alert("Você acertou o número secreto!! Número: " + numeroSecreto);
+        tentativas++;
+    } else {
+        if (chute < numeroSecreto) {
+            alert("O número secreto é maior que: " + chute);
+        } else {
+            alert("O número secreto é menor que: " + chute);
+        }
+        tentativas++;
+    }
+}
+// Criando uma variável para uma mensagem alternativa 
+// e retornando a mensagem personalizada de acordo com a quantidade de acertos
+let tentativasMensagem = tentativas > 1 ? "tentativas." : "tentativa."
+alert("Você descobriu o número secreto " + numeroSecreto + " com " + tentativas + " " + tentativasMensagem);
